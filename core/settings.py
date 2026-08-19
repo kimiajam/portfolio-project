5aee08134a10bddd82b6cb7ba7eeb1a58d190a46
kimiajam <kimiajaam@gmail.com>
2026-08-19 22:09:48 +0330

    Allow Render host

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost",
+    "127.0.0.1",
+    "kimia-portfolio.onrender.com"]
 
 
 # Application definition
